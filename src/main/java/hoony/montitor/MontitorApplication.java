28926ad1888f8cc689fb2a7108ca846de40c0265
donghoony <aru0504@naver.com>
2024-08-04 21:28:03 +0900

    init: Initialize project with dependencies

---
+++ src/main/java/hoony/montitor/MontitorApplication.java
@@ -0,0 +1,12 @@
+package hoony.montitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MontitorApplication.class, args);
+    }
+}
---
+++ src/test/java/hoony/montitor/MontitorApplicationTests.java
@@ -0,0 +1,13 @@
+package hoony.montitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
